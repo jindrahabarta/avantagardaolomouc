837741ccca9a300031886e42a81f58ad28640a0b
jindrahabarta <jindriskuv@email.cz>
2024-11-18 20:17:34 +0100

    link edited to deplozy

--- src/app/_components/Global/Button.tsx
+++ src/app/_components/Global/Button.tsx
@@ -12,7 +12,7 @@ const Button = ({ text, variant, link, className }: props) => {
     return (
       <button
         type="button"
-        aria-label={text}
+        aria-label={text + link}
         className={`border-orange-500 bg-orange-500 border-2 rounded-3xl ${className}`}
       >
         <div className="py-2 px-10 text-xl font-semibold  text-white">
