a38f3a3906a85a4ffc00a98d9ab1afc83911baf8
jindrahabarta <jindriskuv@email.cz>
2025-01-25 12:15:46 +0100

    added dropdown menu

--- src/app/_components/Global/Navigation.tsx
+++ src/app/_components/Global/Navigation.tsx
@@ -8,26 +8,27 @@ import Link from 'next/link'
 import gsap from 'gsap'
 import animationInit from '@/app/assets/animationInit'
 import { usePathname } from 'next/navigation'
+import ChevronDownIcon from '../icons/ChevronDownIcon'
 // import ChevronDownIcon from '../icons/ChevronDownIcon'
 
 const links = [
     {
         text: 'Domů',
         link: '/#uvod',
-        // submenu: [
-        //     {
-        //         text: 'O soláriu',
-        //         link: '/#o-nas',
-        //     },
-        //     {
-        //         text: 'Kabinky',
-        //         link: '/#kabinky',
-        //     },
-        //     {
-        //         text: 'Otázky a odpovědi',
-        //         link: '/#faq',
-        //     },
-        // ],
+        submenu: [
+            {
+                text: 'O soláriu',
+                link: '/#o-nas',
+            },
+            {
+                text: 'Kabinky',
+                link: '/#kabinky',
+            },
+            {
+                text: 'Otázky a odpovědi',
+                link: '/#faq',
+            },
+        ],
     },
     // {
     //     text: 'O soláriu',
@@ -81,10 +82,10 @@ const Navigation = () => {
             .fromTo(
                 '.navBar',
                 {
-                    height: 65,
+                    height: '5rem',
                 },
                 {
-                    height: 300,
+                    height: 320,
                     duration: 0.2,
                 }
             )
@@ -113,8 +114,8 @@ const Navigation = () => {
 
     return (
         <header className='fixed top-0 left-0 w-full flex justify-center px-2 sm:px-5 pt-2 z-50'>
-            <div className='navBar max-w-[1392px] w-full px-4 py-2 bg-white duration-200 border-b rounded-2xl border-black/20 border shadow-md overflow-hidden sm:overflow-visible'>
-                <nav className='w-full flex justify-between items-center gap-2 py-2'>
+            <div className='navBar max-w-[1392px] w-full px-4  bg-white duration-200 border-b rounded-2xl border-black/20 border shadow-md overflow-hidden sm:overflow-visible'>
+                <nav className='w-full flex justify-between items-center gap-2 h-20'>
                     <Link href={'/#uvod'}>
                         <Image
                             src={Logo}
@@ -125,68 +126,77 @@ const Navigation = () => {
                         ></Image>
                     </Link>
 
-                    <div className='sm:flex hidden gap-2 md:gap-10  items-center'>
-                        <ul className='gap-4 flex items-center'>
+                    <div className='sm:flex hidden gap-2 md:gap-10 h-full items-center'>
+                        <ul className='gap-4 h-full flex items-center'>
                             {links.map((link, i) => {
-                                return (
-                                    <li
-                                        key={i}
-                                        className=' text-md sm:text-base lg:text-lg text-nowrap font-semibold'
-                                    >
-                                        <Link
-                                            className='text-black hover:text-orange duration-200'
-                                            href={link.link}
-                                        >
-                                            {link.text}
-                                        </Link>
-                                    </li>
-                                )
-                                // if (link.submenu) {
-                                //     return (
-                                //         <div className='relative' key={i}>
-                                //             <li className='text-md md:text-lg lg:text-xl text-nowrap font-semibold flex items-center gap-1 group'>
-                                //                 <Link
-                                //                     className='text-black hover:text-orange duration-200'
-                                //                     href={link.link}
-                                //                 >
-                                //                     {link.text}
-                                //                 </Link>
-                                //                 <ChevronDownIcon className='w-4 group-hover:stroke-orange stroke-black group-hover:pt-1 duration-300'></ChevronDownIcon>
-
-                                //                 <ul className='w-fit hidden group-hover:block h-fit rounded-2xl p-4 bg-white shadow-md border-black/80 border top-10 -translate-x-[50%] absolute left-[50%]'>
-                                //                     {link.submenu.map(
-                                //                         (submenuLink, i) => (
-                                //                             <li key={i}>
-                                //                                 <Link
-                                //                                     href={
-                                //                                         submenuLink.link
-                                //                                     }
-                                //                                 >
-                                //                                     {
-                                //                                         submenuLink.text
-                                //                                     }
-                                //                                 </Link>
-                                //                             </li>
-                                //                         )
-                                //                     )}
-                                //                 </ul>
-                                //             </li>
-                                //         </div>
-                                //     )
-                                // } else {
-                                //     return (
-                                //         <li
-                                //             key={i}
-                                //             className=' text-md md:text-lg lg:text-xl text-nowrap font-semibold'
+                                // return (
+                                //     <li
+                                //         key={i}
+                                //         className=' text-md sm:text-base lg:text-lg text-nowrap font-semibold'
+                                //     >
+                                //         <Link
+                                //             className='text-black hover:text-orange duration-200'
+                                //             href={link.link}
                                 //         >
-                                //             <Link
-                                //                 className='text-black hover:text-orange duration-200'
-                                //                 href={link.link}
-                                //             >
-                                //                 {link.text}
-                                //             </Link>
-                                //         </li>
-                                //     )
+                                //             {link.text}
+                                //         </Link>
+                                //     </li>
+                                // )
+                                if (link.submenu) {
+                                    return (
+                                        <div
+                                            className='relative group h-full flex items-center'
+                                            key={i}
+                                        >
+                                            <li className='text-md md:text-lg lg:text-xl text-nowrap font-semibold flex items-center cursor-pointer gap-1'>
+                                                <Link
+                                                    className='text-black py-2 group-hover:text-orange duration-200'
+                                                    href={link.link}
+                                                >
+                                                    {link.text}
+                                                </Link>
+                                                <ChevronDownIcon className='w-4 group-hover:stroke-orange stroke-black group-hover:pt-1 duration-300'></ChevronDownIcon>
+                                                <div className='h-fit pt-1 top-20 absolute left-0'>
+                                                    <ul className='w-fit hidden  group-hover:block h-fit rounded-xl p-4 bg-white shadow-md border-black/50 border '>
+                                                        {link.submenu.map(
+                                                            (
+                                                                submenuLink,
+                                                                i
+                                                            ) => (
+                                                                <li key={i}>
+                                                                    <Link
+                                                                        className='text-black hover:text-orange'
+                                                                        href={
+                                                                            submenuLink.link
+                                                                        }
+                                                                    >
+                                                                        {
+                                                                            submenuLink.text
+                                                                        }
+                                                                    </Link>
+                                                                </li>
+                                                            )
+                                                        )}
+                                                    </ul>
+                                                </div>
+                                            </li>
+                                        </div>
+                                    )
+                                } else {
+                                    return (
+                                        <li
+                                            key={i}
+                                            className=' text-md md:text-lg lg:text-xl text-nowrap font-semibold'
+                                        >
+                                            <Link
+                                                className='text-black hover:text-orange duration-200'
+                                                href={link.link}
+                                            >
+                                                {link.text}
+                                            </Link>
+                                        </li>
+                                    )
+                                }
                             })}
                         </ul>
 
